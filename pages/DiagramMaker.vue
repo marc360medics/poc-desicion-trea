6a7bb8402eaa9c6c290b581b993db05dbc34354a
Rosset <theo.rosset@outlook.fr>
2023-02-07 11:46:53 +0100

    refacto(page): delet header

--- pages/DiagramMaker.vue
+++ pages/DiagramMaker.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
-    <HeaderComponent title="DiagramMaker"/>
   </div>
 </template>
 
 <script>
-import HeaderComponent from '~/components/HeaderComponent.vue';
 
 
 export default {
   name:'DiagramMaker',
-  components: { HeaderComponent }
+  components: {  }
 }
 </script>
 
--- pages/Flume.vue
+++ pages/Flume.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <HeaderComponent title="Flume" />
   </div>
 </template>
 
 <script>
-import HeaderComponent from '~/components/HeaderComponent.vue';
 
 export default {
   name: 'Flume',
-  components: { HeaderComponent }
+  components: {  }
 }
 </script>
 
